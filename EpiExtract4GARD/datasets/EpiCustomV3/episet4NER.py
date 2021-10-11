abf7f377716c6df8aac4fd764b3c762c0d940eca
William Kariampuzha <86256217+wzkariampuzha@users.noreply.github.com>
2021-10-11 15:56:45 -0500

    Update episet4NER.py

--- EpiExtract4GARD/datasets/EpiCustomV3/episet4NER.py
+++ EpiExtract4GARD/datasets/EpiCustomV3/episet4NER.py
@@ -34,18 +34,17 @@ _CITATION = """\
 
 _DESCRIPTION = """\
 **REWRITE*
-CoNLLpp is a corrected version of the CoNLL2003 NER dataset where labels of 5.38% of the sentences in the test set
-have been manually corrected. The training set and development set are included for completeness.
-For more details see https://www.aclweb.org/anthology/D19-1519/ and https://github.com/ZihanWangKi/CrossWeigh
+EpiSet4NER is a dataset generated from 620 rare disease abstracts labeled using statistical and rule-base methods. The test set was then manually corrected by a rare disease expert.
+For more details see *INSERT PAPER* and https://github.com/ncats/epi4GARD/tree/master/EpiExtract4GARD#epiextract4gard
 """
 
-_URL = "https://github.com/ZihanWangKi/CrossWeigh/raw/master/data/"
-_TRAINING_FILE = "conllpp_train.txt"
-_DEV_FILE = "conllpp_dev.txt"
-_TEST_FILE = "conllpp_test.txt"
+_URL = "https://github.com/NCATS/epi4GARD/tree/master/EpiExtract4GARD/datasets/EpiCustomV3"
+_TRAINING_FILE = "train.txt"
+_VAL_FILE = "val.txt"
+_TEST_FILE = "test.txt"
 
 
-class ConllppConfig(datasets.BuilderConfig):
+class EpiSetConfig(datasets.BuilderConfig):
     """BuilderConfig for Conll2003"""
 
     def __init__(self, **kwargs):
@@ -56,11 +55,11 @@ class ConllppConfig(datasets.BuilderConfig):
         super(ConllppConfig, self).__init__(**kwargs)
 
 
-class Conllpp(datasets.GeneratorBasedBuilder):
-    """Conllpp dataset."""
+class EpiSet(datasets.GeneratorBasedBuilder):
+    """EpiSet4NER by GARD."""
 
     BUILDER_CONFIGS = [
-        ConllppConfig(name="conllpp", version=datasets.Version("1.0.0"), description="Conllpp dataset"),
+        EpiSetConfig(name="EpiSet4NER", version=datasets.Version("1.0.0"), description="EpiSet4NER by NIH NCATS GARD"),
     ]
 
     def _info(self):
@@ -70,107 +69,24 @@ class Conllpp(datasets.GeneratorBasedBuilder):
                 {
                     "id": datasets.Value("string"),
                     "tokens": datasets.Sequence(datasets.Value("string")),
-                    "pos_tags": datasets.Sequence(
-                        datasets.features.ClassLabel(
-                            names=[
-                                '"',
-                                "''",
-                                "#",
-                                "$",
-                                "(",
-                                ")",
-                                ",",
-                                ".",
-                                ":",
-                                "``",
-                                "CC",
-                                "CD",
-                                "DT",
-                                "EX",
-                                "FW",
-                                "IN",
-                                "JJ",
-                                "JJR",
-                                "JJS",
-                                "LS",
-                                "MD",
-                                "NN",
-                                "NNP",
-                                "NNPS",
-                                "NNS",
-                                "NN|SYM",
-                                "PDT",
-                                "POS",
-                                "PRP",
-                                "PRP$",
-                                "RB",
-                                "RBR",
-                                "RBS",
-                                "RP",
-                                "SYM",
-                                "TO",
-                                "UH",
-                                "VB",
-                                "VBD",
-                                "VBG",
-                                "VBN",
-                                "VBP",
-                                "VBZ",
-                                "WDT",
-                                "WP",
-                                "WP$",
-                                "WRB",
-                            ]
-                        )
-                    ),
-                    "chunk_tags": datasets.Sequence(
-                        datasets.features.ClassLabel(
-                            names=[
-                                "O",
-                                "B-ADJP",
-                                "I-ADJP",
-                                "B-ADVP",
-                                "I-ADVP",
-                                "B-CONJP",
-                                "I-CONJP",
-                                "B-INTJ",
-                                "I-INTJ",
-                                "B-LST",
-                                "I-LST",
-                                "B-NP",
-                                "I-NP",
-                                "B-PP",
-                                "I-PP",
-                                "B-PRT",
-                                "I-PRT",
-                                "B-SBAR",
-                                "I-SBAR",
-                                "B-UCP",
-                                "I-UCP",
-                                "B-VP",
-                                "I-VP",
-                            ]
-                        )
-                    ),
                     "ner_tags": datasets.Sequence(
                         datasets.features.ClassLabel(
                             names=[
                                 "O",
-                                "B-PER",
-                                "I-PER",
-                                "B-ORG",
-                                "I-ORG",
+                                "B-EPI",
                                 "B-LOC",
+                                "B-STAT",
+                                "I-EPI",
                                 "I-LOC",
-                                "B-MISC",
-                                "I-MISC",
+                                "I-STAT",
+                                "O",
                             ]
                         )
                     ),
                 }
             ),
             supervised_keys=None,
-            homepage="https://github.com/ZihanWangKi/CrossWeigh",
+            homepage="https://github.com/ncats/epi4GARD/tree/master/EpiExtract4GARD#epiextract4gard",
             citation=_CITATION,
         )
 
@@ -178,14 +94,14 @@ class Conllpp(datasets.GeneratorBasedBuilder):
         """Returns SplitGenerators."""
         urls_to_download = {
             "train": f"{_URL}{_TRAINING_FILE}",
-            "dev": f"{_URL}{_DEV_FILE}",
+            "val": f"{_URL}{_VAL_FILE}",
             "test": f"{_URL}{_TEST_FILE}",
         }
         downloaded_files = dl_manager.download_and_extract(urls_to_download)
 
         return [
             datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": downloaded_files["train"]}),
-            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={"filepath": downloaded_files["dev"]}),
+            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={"filepath": downloaded_files["val"]}),
             datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"filepath": downloaded_files["test"]}),
         ]
 
@@ -194,8 +110,6 @@ class Conllpp(datasets.GeneratorBasedBuilder):
         with open(filepath, encoding="utf-8") as f:
             guid = 0
             tokens = []
-            pos_tags = []
-            chunk_tags = []
             ner_tags = []
             for line in f:
                 if line.startswith("-DOCSTART-") or line == "" or line == "\n":
@@ -203,28 +117,20 @@ class Conllpp(datasets.GeneratorBasedBuilder):
                         yield guid, {
                             "id": str(guid),
                             "tokens": tokens,
-                            "pos_tags": pos_tags,
-                            "chunk_tags": chunk_tags,
                             "ner_tags": ner_tags,
                         }
                         guid += 1
                         tokens = []
-                        pos_tags = []
-                        chunk_tags = []
                         ner_tags = []
                 else:
-                    # conll2003 tokens are space separated
+                    # EpiSet tokens are space separated
                     splits = line.split(" ")
                     tokens.append(splits[0])
-                    pos_tags.append(splits[1])
-                    chunk_tags.append(splits[2])
-                    ner_tags.append(splits[3].rstrip())
+                    ner_tags.append(splits[1].rstrip())
             # last example
             if tokens:
                 yield guid, {
                     "id": str(guid),
                     "tokens": tokens,
-                    "pos_tags": pos_tags,
-                    "chunk_tags": chunk_tags,
                     "ner_tags": ner_tags,
                 }
